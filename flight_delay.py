13f16b256aa74dca8a99f5df5a6d14d48fbb7067
imsansanich <sashulia731@gmail.com>
2025-06-12 10:57:04 +0200

    Update flight_delay.py

--- flight_delay.py
+++ flight_delay.py
@@ -38,18 +38,6 @@ app.layout = html.Div(children=[ html.H1('Flight Delay Time Statistics',
                                 html.Div(dcc.Graph(id='late-plot'), style={'width':'65%'})
                                 ])
 
-""" Compute_info function description
-
-This function takes in airline data and selected year as an input and performs computation for creating charts and plots.
-
-Arguments:
-    airline_data: Input airline data.
-    entered_year: Input year for which computation needs to be performed.
-    
-Returns:
-    Computed average dataframes for carrier delay, weather delay, NAS delay, security delay, and late aircraft delay.
-
-"""
 def compute_info(airline_data, entered_year):
     # Select data
     df =  airline_data[airline_data['Year']==int(entered_year)]
@@ -61,18 +49,6 @@ def compute_info(airline_data, entered_year):
     avg_late = df.groupby(['Month','Reporting_Airline'])['LateAircraftDelay'].mean().reset_index()
     return avg_car, avg_weather, avg_NAS, avg_sec, avg_late
 
-"""Callback Function
-
-Function that returns fugures using the provided input year.
-
-Arguments:
-
-    entered_year: Input year provided by the user.
-    
-Returns:
-
-    List of figures computed using the provided helper function `compute_info`.
-"""
 # Callback decorator
 @app.callback( [
                Output(component_id='carrier-plot', component_property='figure'),
